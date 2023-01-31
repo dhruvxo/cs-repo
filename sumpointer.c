93e65098a6f2f8bb762afaeaa55622c304e09a6a
Dhruv <drutusofficial@gmail.com>
2023-01-31 21:26:11 +0530

    calloc c program

---
+++ sumpointer.c
@@ -0,0 +1,27 @@
+//sum of 3 numbers using pointers
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,sum=0,*ptr;
+    printf("enter no of elements:");
+    scanf("%d",&n);
+    ptr= calloc(n,sizeof(int));
+    if(ptr==NULL)
+    {
+        printf("memory not allocated\n");
+        exit(0);
+    }
+    printf("Enter elements\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",ptr(i));
+    }
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",ptr+i);
+        sum = sum + ptr[i];
+    }
+     printf("sum=%d",sum);
+     return 0;
+    }
