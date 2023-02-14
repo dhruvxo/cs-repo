dd3862fc5de95db7ae7652c3dbccb2c6955cbad5
dhruv <drutusofficial@gmail.com>
2023-02-14 10:56:40 +0530

    js

--- C Programs/csll.c
+++ C Programs/csll.c
@@ -47,51 +47,51 @@ void initList(CLL *cll)
     temp->next=cll->last->next;
     cll->last->next=temp;
 }
-// void insertLast(CLL* cll,int ele)
-// {
-// 	NODE *temp=getNode(ele);
-// 	if(cll->head==NULL)
-// 	{
-// 		cll->head=temp;
-// 		return;
-// 	} 
-// 	NODE *p=cll->head;
-// 	while(p->next!=NULL)
-// 		p=p->next;
-// 	p->next=temp;
-// }
-// int countnodes(CLL *cll)
-// {
-// 	int count=0;
-// 	if(cll->head==NULL)
-// 		return 0;
-// 	NODE *p=cll->head;
-// 	while(p!=NULL)
-// 	{
-// 		count++;
-// 		p=p->next;
-// 	}
-// 	return count;
-// }
-// void insertAtPos(CLL *cll,int ele,int pos)
-// {
-// 	//int n=countnodes(cll);
-// 	NODE *temp=getNode(ele);
-// 	if(pos==0)
-// 	{
-// 		temp->next=cll->head;
-// 		cll->head=temp;
-// 		return;
-// 	}
-// 	NODE *p=cll->head, *q=NULL;
-// 	for(int i=1;i<pos;i++)
-// 	{
-// 		q=p;
-// 		p=p->next;
-// 	}
-// 	q->next=temp;
-// 	temp->next=p;
-// }
+void insertLast(CLL* cll,int ele)
+{
+	NODE *temp=getNode(ele);
+	if(cll->head==NULL)
+	{
+		cll->head=temp;
+		return;
+	} 
+	NODE *p=cll->head;
+	while(p->next!=NULL)
+		p=p->next;
+	p->next=temp;
+}
+int countnodes(CLL *cll)
+{
+	int count=0;
+	if(cll->head==NULL)
+		return 0;
+	NODE *p=cll->head;
+	while(p!=NULL)
+	{
+		count++;
+		p=p->next;
+	}
+	return count;
+}
+void insertAtPos(CLL *cll,int ele,int pos)
+{
+	//int n=countnodes(cll);
+	NODE *temp=getNode(ele);
+	if(pos==0)
+	{
+		temp->next=cll->last->next;
+		cll->last->next=temp;
+		return;
+	}
+	NODE *p=cll->last->next, *q=NULL;
+	for(int i=1;i<pos;i++)
+	{
+		q=p;
+		p=p->next;
+	}
+	q->next=temp;
+	temp->next=p;
+}
 void display(CLL* cll)
 {
 	if(cll->last==NULL)
@@ -136,18 +136,18 @@ int main()
 				   scanf("%d",&ele);
 				   insertFront(&lobj,ele);
 				   break;
-			// case 2:printf("enter an integer\n");
-			// 	   scanf("%d",&ele);
-			// 	   insertLast(&lobj,ele);
-			// 	   break;
-			// case 3:printf("enter an integer\n");
-			// 	   scanf("%d",&ele);
-			// 	   printf("enter the pos:");
-			// 	   scanf("%d",&pos);
-			// 	   insertAtPos(&lobj,ele,pos);
-			// 	   break;
-			// case 4:display(&lobj);
-			// 	   break;
+			case 2:printf("enter an integer\n");
+				   scanf("%d",&ele);
+				   insertLast(&lobj,ele);
+				   break;
+			case 3:printf("enter an integer\n");
+				   scanf("%d",&ele);
+				   printf("enter the pos:");
+				   scanf("%d",&pos);
+				   insertAtPos(&lobj,ele,pos);
+				   break;
+			case 4:display(&lobj);
+				   break;
 	}
   }while(choice<5);
   destroyList(&lobj);
