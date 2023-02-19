90244118d8d9d1c0c74832e05003a869ed1facd3
dhruv <drutusofficial@gmail.com>
2023-02-19 19:04:01 +0530

    portfolio

---
+++ Portfolio1/portfolio-master/js/project_data.js
@@ -0,0 +1,62 @@
+const ProjectData = {
+	"growth_tracker": {
+		title: "Growth Tracker",
+		github: "https://github.com/ottomated/growth-tracker",
+		description: "Growth Tracker is a <a href=\"https://discordapp.com/\">Discord</a> bot that's currently used by over 2,000 servers and 1,000,000 total people. It tracks how many people are in each server over time and can generate and display graphs of this growth. This was the first project that I made that became widely used, and I'm still maintaining it alone today."
+	},
+	"dickens": {
+		title: "Dickens Carolers",
+		github: "",
+		description: "This is a private project I did under contract from the <a href=\"http://www.dickenscarolers.com/\">Dickens Carolers</a>, a Seattle singing group. It involved interfacing with a Google spreadsheet and managing scheduling for dozens of people."
+	},
+	"minesweeper_b": {
+		title: "Minesweeper Royale (backend)",
+		github: "https://github.com/WilliamDann/minesweeper-royale",
+		description: "During CodeDay Spring 2019, a 24-hour hackathon, I created the Express backend for an online battle royale version of minesweeper, using websockets to communicate."
+	},
+	"dunabase": {
+		title: "Dunabase",
+		github: "https://github.com/ottomated/dunabase",
+		description: "My first venture into Flutter. This app connects with a Google spreadsheet and uses an algorithm to enumerate the songs that can be sung by a subset of a singing group (in which each person only knows a certain set of songs)"
+	},
+	"source_mobile": {
+		title: "Source Mobile",
+		github: "https://github.com/ottomated/source-mobile",
+		description: "An app that allows students of Seattle Public Schools to check their grades on their mobile device. Published on the Apple Store and Play Store with over 3,000 downloads and great feedback! I also created a splash website from scratch for this project <a href=\"https://ottomated.net/source\">here</a>."
+	},
+	"repertoire": {
+		title: "Repertoire",
+		github: "https://github.com/ottomated/repertoire",
+		description: "I made this app for my dance group to ease the sharing of costumes and dance data. This is the first project where I actively pursued usability testing and I plan to publish it once it's polished."
+	},
+	"makemehappy": {
+		title: "Make Me Happy",
+		github: "https://github.com/WilliamDann/hack2018/tree/master/CodeDay/static",
+		description: "An app made in 24 hours for CodeDay Fall 2018. I worked on the frontend and used this project to learn about how to use git properly."
+	},
+	"portfolio": {
+		title: "Portfolio",
+		github: "https://github.com/ottomated/portfolio",
+		description: "You're looking at it! This website was a lot of work and I'm really proud of some of the results I got, such as the svg animations and the overall aesthetic."
+	},
+	"minesweeper_f": {
+		title: "Minesweeper Royale (frontend)",
+		github: "https://github.com/WilliamDann/minesweeper-royale/tree/master/public",
+		description: "Along with the Express backend for this 24-hour project, I created the HTML/CSS/JS frontend! I think it turned out really well for the limited time, and matched the original theme accurately.",
+	},
+	"antislouch": {
+		title: "AntiSlouch",
+		github: "https://github.com/ottomated/AntiSlouch",
+		description: "Made in 24 hours at CodeDay Spring 2018, this app uses face tracking in OpenCV to detect whether or not you're slouching at your computer. Made in Python.",
+	},
+	"reqlang": {
+		title: "ReqLang",
+		github: "https://github.com/ottomated/reqlang",
+		description: "How I got to know C++. The eventual goal of this project is to create an interpreter for a custom language based around scripting HTTP requests, but I can't devote much time to it and it's nowhere near complete.",
+	},
+	"ognes": {
+		title: "OGNes",
+		github: "https://github.com/ottomated/OGNes",
+		description: "An NES emulator written in Java for a school project. It's compatible with most common games, although definitely not perfect.",
+	},
+};
