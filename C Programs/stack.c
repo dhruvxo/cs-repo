6394eda7a2f7f976472955e0a2929f3dfc46eb57
dhruv <drutusofficial@gmail.com>
2023-02-20 11:49:03 +0530

    stack

--- C Programs/stack.c
+++ C Programs/stack.c
@@ -13,12 +13,12 @@ ps->top=-1;
 
 int isFull(STACK *ps)
 {
-    return ps->top==MAX-1 //if it returns 0, stack isnt full
+    return ps->top==MAX-1; //if it returns 0, stack isnt full
 }
 
 int isEmpty(STACK *ps)
 {
-    return ps->top==-1 //if it returns 0, stack isnt empty
+    return ps->top==-1; //if it returns 0, stack isnt empty
 }
 
 int push(STACK *ps, int ele)
@@ -28,7 +28,6 @@ int push(STACK *ps, int ele)
     return 0;
     ps->top++;
     ps->s[ps->top]=ele;
-    //ps->s[++ps->top]
     return 1;
 }
 
@@ -40,8 +39,7 @@ int pop(STACK *ps, int *pe) //*pe is used to store the element which gets popped
         return 0;
     }
     *pe=ps->s[ps->top];
-    ps->s[ps->top--];
-    //*pe=ps->s[ps->top--];
+    ps->top--;
     return 1;
 }
 
@@ -49,7 +47,6 @@ int peek(STACK *ps, int *pe)
 {
      if(isEmpty(ps))
     {
-        //printf("Stack Underflow! (No elements)");
         return 0;
     }
     *pe=ps->s[ps->top];
@@ -60,10 +57,68 @@ void display(STACK *ps)
 {
     if(isEmpty(ps))
     {
-        printf("Stack Underflow!")
+        printf("Stack is Empty.");
+        return;
     }
     for(int i=ps->top;i>-1;i--)
     {
         printf("%d",ps->s[i]);
+        printf("\n");
     }
+}
+
+int main()
+{
+  STACK s;
+  int choice,ele;
+  
+	  printf("\n 1.Push\n 2.Pop\n 3.Peek\n 4.isEmpty\n 5.isFull\n 6.Display\n");
+	  printf("enter your choice:");
+	  scanf("%d",&choice);
+    do
+	{
+	  switch(choice)
+	  {
+		    case 1:printf("enter the element to be pushed:\n");
+				   scanf("%d",&ele);
+                   if(push(&s,ele))
+				   printf("Pushed %d",ele);
+                   else
+                   printf("Stack Overflow!\n");
+				   break;
+
+			case 2://printf("enter the element to be popped:\n");
+                    //scanf("%d",&ele);
+                    if(pop(&s,&ele)) //address of element must be used because pass by reference because whatever is there in *pe will be popped instead of its actual value
+                    printf("Popped %d",ele);
+                    else
+                    printf("Stack Underflow!\n");
+                    break;
+
+            case 3: if(peek(&s,&ele))
+                    printf("stack top is %d",ele);
+                    else
+                    printf("Stack is Empty\n");
+                    break;
+
+            case 4: if(isEmpty(&s))
+                    printf("stack is empty\n");
+                    else
+                    printf("stack contains elements");
+                    break;
+
+            case 5: if(isFull(&s))
+                    printf("Stack is Full\n");
+                    else
+                    printf("Stack isn't full\n");
+                    break;
+
+			case 6:display(&s);
+				   break;
+	  }
+      printf("\n 1.Push\n 2.Pop\n 3.Peek\n 4.isEmpty\n 5.isFull\n 6.Display\n");
+	  printf("enter your choice:");
+	  scanf("%d",&choice);
+  }while(choice<7);
+  return 0;
 }
