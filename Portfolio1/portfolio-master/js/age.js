9d4549e3626e126e1693ffd39d2970b430795b43
dhruv <drutusofficial@gmail.com>
2023-03-11 20:57:28 +0530

    age

--- Portfolio1/portfolio-master/js/age.js
+++ Portfolio1/portfolio-master/js/age.js
@@ -1,6 +1,6 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-	let time = (new Date() - new Date(2003,0,1)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+	let time = (new Date() - new Date(1678548358)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
 	ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
