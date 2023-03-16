50309dd2d68900506d5ed88882a0e03033bb77d2
dhruvxo <drutusofficial@gmail.com>
2023-03-16 15:56:14 +0530

    priority queue

---
+++ C Programs/priorityQ.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define max 50
+typedef struct priq
+{
+    int pq[max];
+    int n;
+}PQ;
+void init(PQ *pt)
+{
+    pt->n=0;
+}
+void display(PQ *pt)
+{
+    
+}
--- C Programs/topdownHeap.c
+++ C Programs/topdownHeap.c
@@ -2,16 +2,28 @@
 #define max 10
 int main()
 {
-    int a[max],i,c,p,n,elt;
+    int a[max];
+    int i,c,p,n,elt;
     printf("enter number of elements:\n");
     scanf("%d",&n);
     printf("Enter elements:\n");
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
     scanf("%d",&a[i]);
-    //heapify
-    heapify(arr);
+    for(i=0;i<n;i++)
+    {
+        elt=a[i];
+        c=i;
+        p=(c-1)/2;
+        while(c>0 && a[p]<elt)
+        {
+            a[c]=a[p];
+            c=p;
+            p=(c-1)/2;
+        }
+        a[c]=elt;
+    }
     printf("Sorted elements in the array are:\n");
-    for(i=1;i<=n;i++)
-    printf("%d\n",arr[i]);
+    for(i=0;i<n;i++)
+    printf("%d\n",a[i]);
     return 0;
 }
