3e21fdd903626c062f565fab37847b9cfdc9fc81
dhruvxo <drutusofficial@gmail.com>
2023-03-21 11:51:32 +0530

    DFS

--- node/n1.js
+++ node/n1.js
@@ -1,4 +1,20 @@
-console.log("Hello World")
+// console.log("Hello World")
 
-const name1="Dhruv"
-console.log("Hello",name1)
+// const name1="Dhruv"
+// console.log("Hello",name1)
+
+const fs=require('fs')
+fs.readFile("randomfile.js", function(err,data){
+    if(err)
+    {
+        console.log(err)
+    }
+    setTimeout(()=>{
+        console.log("PES University display after 2 seconds")
+    },2000)
+})
+console.log("Starts here")
+
+// setTimeout(()=>{
+//     console.log("Timer Stopped")},2000)
+//     console.log("Timer Started")
