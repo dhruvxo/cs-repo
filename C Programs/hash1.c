930fb0472e0d625cfb6b0e8d8ce82b3c56444b36
dhruvxo <drutusofficial@gmail.com>
2023-03-31 16:25:19 +0530

    hash

--- C Programs/hash1.c
+++ C Programs/hash1.c
@@ -12,7 +12,33 @@ typedef struct student
 void initTable(NODE ht[size],int *n)
 {
     for(int i=0;i<size;i++)
+    ht[i].mark=0;
+    (*n)=0;
+}
+
+int insert(NODE ht[size],int Rno,char name[30],int *n)
+{
+    if(*n==size)
+    return 0;
+    int index=Rno%size;
+    while(ht[index].mark!=0)
+    index=(index+1)%size;
+    ht[index].Rno=Rno;
+    strcpy(ht[index].name,name);
+    ht[index].mark=1;
+    (*n)++;
+    return 1;
+}
+
+int delete(NODE ht[size],int Rno,int *n)
+{
+    if(*n==0)
+    return 0;
+    int index=Rno%size;
+    int i=1;
+    while(ht[index].Rno!=Rno&&i<*n)
     {
+        index=(index+i)%size;
         
     }
 }
