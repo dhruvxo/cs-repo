c23289fbbfec5ba800b0fee2dfa7be906c85f6be
dhruvxo <drutusofficial@gmail.com>
2023-06-07 11:55:24 +0530

    subroutine

--- ARMSim/SubRoutine.s
+++ ARMSim/SubRoutine.s
@@ -1,1 +1,12 @@
-@@Write an assembly language program using arm instruction to complete the following using sub-routines: factorial, 
+@@Write an assembly language program using arm instruction to complete the following using sub-routines: factorial, GCD
+
+.DATA
+MOV R0,#12
+MOV R1,#1
+BL FACTORIAL
+SWI 0X011
+
+FACTORIAL:
+MUL R1,R0,R1
+SUBS R0,R0,#1
+BNE FACTORIAL
