7aefdbf905d380c2fa23f39c120d618748897c6d
dhruvxo <drutusofficial@gmail.com>
2023-06-07 11:03:24 +0530

    fibonacci

---
+++ ARMSim/fibonacci.s
@@ -0,0 +1,22 @@
+@@generate fibonacci series and store them in an array
+
+.DATA
+fibonacci: .WORD 0,0,0,0,0,0,0,0,0,0
+
+.TEXT
+MOV R0,#0
+MOV R1,#1
+LDR R2,=fibonacci
+
+STR R0,[R2],#4
+STR R1,[R2],#4
+MOV R3,#8
+
+CMP R3,#0
+BEQ L1
+ADD R2,R0,R1
+STR R4,[R2],#4
+MOV R0,R1
+MOV R1,R4
+SUB R3,R3,#1
+L1: SWI 0X011
