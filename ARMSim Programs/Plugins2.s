ad2ca5ace6406c7aa28c6c9c604747f15c813113
dhruvxo <drutusofficial@gmail.com>
2023-07-12 12:36:05 +0530

    plugins

--- ARMSim Programs/Plugins2.s
+++ ARMSim Programs/Plugins2.s
@@ -10,7 +10,7 @@ cmp r0, #2  ;left button - downcounter
 beq loop2
 b again
 loop1: mov r5,#16
-ldr r1,=zero
+ldr r1,=U
 back1:ldrb r0,[r1]
 swi 0x200  ;Set 8 segment display to light
 bl delay
@@ -20,9 +20,10 @@ cmp r5,#0
 bne back1
 b again
 
+
 loop2:
 mov r5,#16
-ldr r1,=F
+ldr r1,=A
 back2: ldrb r0,[r1]
 swi 0x200  ;Set 8 segment to light up
 bl delay
@@ -33,25 +34,32 @@ bne back2
 b again
 
 delay: mov r4,#64000
+
 loop3: sub r4,r4,#1
 cmp r4, #0
 bge loop3
 mov pc,lr
 
 .data
-zero: .byte 0b11101101
-one: .byte 0b01100000
-two: .byte 0b01101110
-three: .byte 0b11111010
-four: .byte 0b00110011
-five: .byte 0b10101011
-six: .byte 0b10101111
-seven: .byte 0b01110000
-eight: .byte 0b11101111
-nine: .byte 0b11100011
+;zero: .byte 0b11101101
+;one: .byte 0b01100000
+;two: .byte 0b01101110
+;three: .byte 0b11111010
+;four: .byte 0b00110011
+;five: .byte 0b10101011
+;six: .byte 0b10101111
+;seven: .byte 0b01110000
+;eight: .byte 0b11101111
+;nine: .byte 0b11100011
+
+;abcpdefg
+U:.byte   0b01101101
+L:.byte 0b00001101
+;L:.byte 0b00001101
 A: .byte 0b11100111
-B: .byte 0b00101111
-C: .byte 0b10001101
-D: .byte 0b01101110
-E: .byte 0b10001111
-F: .byte 0b10000111
+S: .byte 0b10101011
+;B: .byte 0b00101111
+;C: .byte 0b10001101
+;D: .byte 0b01101110
+;E: .byte 0b10001111
+;F: .byte 0b10000111
