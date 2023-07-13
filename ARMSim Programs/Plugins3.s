4cdb169292e9955474b113a7b4db984955eebc11
dhruvxo <drutusofficial@gmail.com>
2023-07-13 08:40:55 +0530

    PLUGINS

--- ARMSim Programs/Plugins3.s
+++ ARMSim Programs/Plugins3.s
@@ -6,6 +6,7 @@ mov r7,#0
 ldr r8,=num
 ldr r8,[r8]
 ldr r2,=str
+
 loop:
 swi 0x204 ;display a string on a screen (R2:address)
 bl sum
@@ -13,6 +14,7 @@ cmp r0,#0
 subne r0,r0,#1
 swieq 0x11
 b loop
+
 sum: cmp r7,r8
 addne r7,r7,#1
 bne sum
