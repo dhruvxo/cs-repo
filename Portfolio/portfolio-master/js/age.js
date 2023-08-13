edb06e3a01c6104d9cb093ed10e2e4e7ac623dcd
dhruvxo <drutusofficial@gmail.com>
2023-08-13 20:56:13 +0530

    portf

--- Portfolio/portfolio-master/js/age.js
+++ Portfolio/portfolio-master/js/age.js
@@ -1,6 +1,6 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-	let time = (new Date() - new Date(1678548358)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+	let time = (new Date() - new Date(1071372800000)) / (1000 * 60 * 60 * 24 * 365.25);
 	ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
