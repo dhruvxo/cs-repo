89470d300cc0bc52c2400bbf5b8dfd29854c2f1b
dhruvxo <drutusofficial@gmail.com>
2024-07-31 11:50:25 +0530

    arrays

---
+++ rust-programs/array.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let arr:[i32;3] = [-1;3];
+    println!("array is {:?}",arr);
+}
+
+// fn main(){
+//     let fruits = ["Apple","Banana","Cherry"];
+//     for fruit in fruits.iter(){
+//         println!("{}",fruit);
+//     }
+// }
--- rust-programs/tempCodeRunnerFile.rs
+++ rust-programs/tempCodeRunnerFile.rs
@@ -1,30 +1,7 @@
-use rand::Rng;
-use std::io;
+fn main(){
+    let fruits = ["Apple","Banana","cherry"];
 
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut attempts = 0;
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        attempts += 1;
-
-        if guess < secret_number {
-            println!("Too low!");
-        } else if guess > secret_number {
-            println!("Too high!");
-        } else {
-            println!("You guessed it in {} attempts!", attempts);
-            break;
-        }
+    for fruit in fruits.iter(){
+        println!("{}",fruit);
     }
 }
